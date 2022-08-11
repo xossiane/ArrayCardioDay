bf9eb7bec25d5623d5d658e9095e4b388c4ba107
xossiane <anejosi.lima@gmail.com>
2022-08-11 18:27:44 -0300

    Exercicios 3 feito

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 import './style.css'
 
+ // Array.prototype.filter()
+    // 1. Filter the list of inventors for those who were born in the 1500's
+
+    // Array.prototype.map()
+    // 2. Give us an array of the inventors first and last names
+
+    // Array.prototype.sort()
+    // 3. Sort the inventors by birthdate, oldest to youngest
+
+    // Array.prototype.reduce()
+    // 4. How many years did all the inventors live all together?
+
+    // 5. Sort the inventors by years lived
+
+    // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+    // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+    // 7. sort Exercise
+    // Sort the people alphabetically by last name
+
+    // 8. Reduce Exercise
+    // Sum up the instances of each of these
 
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
@@ -33,3 +56,9 @@ function BornIn1500(inventor){
   const fullNames = 
      inventors.map(inventor => `${inventor.first} ${inventor.last}`);
      console.log(fullNames)
+
+
+const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1)
+console.table(ordered)
+
+var totalYears = 0;
